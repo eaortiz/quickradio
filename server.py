eccb230725ec6c7f6146079b53e5d03dc14f6e0f
Estefania Ortiz <eaortiz@stanford.edu>
2015-05-23 15:55:49 -0700

    post bug fixes

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, url_for
+import os
 app = Flask(__name__)
 
 
@@ -10,6 +11,6 @@ def hello_world():
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+	app.run(host='0.0.0.0', port=port)
 	url_for('static', filename='04NoCreo.m4a')
 

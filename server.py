93623de57816d1fce868d8f661cbb6f2f20668c2
Estefania Ortiz <eaortiz@stanford.edu>
2015-05-23 17:24:17 -0700

    binary song

--- server.py
+++ server.py
@@ -5,12 +5,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+	# return render_template('04NoCreo.html')
 	return "Click this link for \
-	<a href = '/static/04NoCreo.m4a'> \
+	<a href = '/static/binary'> \
 	the Shakira song. </a>"
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
-	url_for('static', filename='04NoCreo.m4a')
+	url_for('static', filename='binary')
 

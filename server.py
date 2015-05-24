687aacd7ae47dee9460a5c9aca794b79ed8e2f2a
Estefania Ortiz <eaortiz@stanford.edu>
2015-05-23 21:24:54 -0700

    radio turned into smart light

--- server.py
+++ server.py
@@ -1,17 +1,14 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 import os
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-	# return render_template('04NoCreo.html')
-	return "Click this link for \
-	<a href = '/static/binary'> \
-	the Shakira song. </a>"
+	return render_template('ligths.html')
 
 if __name__ == '__main__':
 	port = int(os.environ.get('PORT', 5000))
 	app.run(host='0.0.0.0', port=port)
 	url_for('static', filename='binary')
-
+	url_for('static', filename='binarytiny')
